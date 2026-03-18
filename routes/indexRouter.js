3d19347d4196718243aaf69b6e46c2104af07631
indumatic <electrico@indumatic.net>
2026-03-18 09:25:11 +0100

    add detail view for messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,12 +4,14 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
+    id: 1
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date(), 
+    id: 2
   }
 ];
 
@@ -20,9 +22,15 @@ indexRouter.post("/new", (req, res) => {
     messages.push({
         text: req.body.messageText,
         user: req.body.messageUser,
-        added: new Date()
+        added: new Date(),
+        id:  messages.length + 1
     })
     res.redirect("/")
+})
+indexRouter.get("/messages/:messageId", (req,res) => {
+    const {messageId} = req.params
+    const message = messages.find(message => message.id === Number(messageId))
+    res.render("messages/messageDetail", {message: message})
 })
 
 module.exports = indexRouter
